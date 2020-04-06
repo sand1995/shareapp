5d5d5e77934d7c21f04ad3107dc207020e86b9ff
Andres Santiago Arias P <zant20@gmail.com>
2020-04-05 20:20:13 -0500

    first fixes, multidex, google services

---
+++ lib/pages/timeline.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:share_app/widgets/header.dart';
+
+class Timeline extends StatefulWidget {
+  @override
+  _TimelineState createState() => _TimelineState();
+}
+
+class _TimelineState extends State<Timeline> {
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: header(context, isAppTitle: true),
+      body: Text("Timeline"),
+    );
+  }
+}
